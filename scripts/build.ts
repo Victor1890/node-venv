16e1f410050d442349a5418bc50e6b17a9902320
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2025-01-26 17:17:38 -0400

    build: :construction_worker: add build and shebang scripts; update package.json for entry points and new commands

---
+++ scripts/build.ts
@@ -0,0 +1,23 @@
+import { Generator } from "npm-dts";
+import { readFileSync, writeFileSync } from "fs";
+import { build } from "bun";
+
+new Generator({
+  entry: "src/index.ts",
+  output: "dist/index.d.ts",
+}).generate();
+
+const pkg = JSON.parse(readFileSync("./package.json", "utf-8"));
+
+await build({
+  entrypoints: ["./src/index.ts"],
+  outdir: "./dist",
+  minify: true,
+  external: Object.keys(pkg.dependencies),
+  target: "node",
+  format: "esm",
+});
+
+// Update package.json to use the correct entry point
+pkg.main = "./dist/index.js";
+writeFileSync("./package.json", JSON.stringify(pkg, null, 2));
